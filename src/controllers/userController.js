8f954b0762b013275389a37bf2b54868bb9bad91
DPRW84 <chomicz.martyna@gmail.com>
2024-10-01 22:45:19 +0200

    formatting

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -6,7 +6,6 @@ import {UserSchema} from "../models/userModel";
 const User = mongoose.model('User', UserSchema);
 
 export const loginRequired = (req, res, next) => {
-    console.log("- - - loginRequired START - - - ")
     if (req.user) {
         next();
     } else {
@@ -15,8 +14,6 @@ export const loginRequired = (req, res, next) => {
 };
 
 export const registerUser = async (req, res) => {
-    console.log('- - - registerUser START - - - ');
-
     try {
         const newUser = new User(req.body);
         newUser.hashPassword = bcrypt.hashSync(req.body.password, 10);
@@ -25,15 +22,12 @@ export const registerUser = async (req, res) => {
         console.log('- - - registerUser creating new user - - - ');
         return res.send(newUser);
     } catch (err) {
-        console.log('- - - registerUser some error - - - ');
         return res.status(400).send({message: err});
     }
 };
 
 export const loginUser = async (req, res) => {
-    console.log('- - - loginUser START - - - ');
     const existingUser = await User.findOne({email: req.body.email});
-
     if (!existingUser) {
         console.log('- - - loginUser email not found  - - - ');
         return res.status(401).send({message: 'Authentication failed. No user found.'});
